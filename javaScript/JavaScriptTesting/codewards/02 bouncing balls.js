7824bbacac08e6766775f7e626f3df1cbdd4bfce
wangkaiwd <1162486176@qq.com>
2019-03-12 18:03:24 +0800

    feat: 代码优化

--- javaScript/JavaScriptTesting/codewards/02 bouncing balls.js
+++ javaScript/JavaScriptTesting/codewards/02 bouncing balls.js
@@ -6,15 +6,13 @@
 // 一个孩子在高楼的第n层玩球。楼层高度h已知。
 // 他将球扔出窗外。例如：球会弹起，弹起的高度为之前高度的2/3，即(h * 0.66)
 // (look out:向外看) 他的妈妈 从离地1.5高的窗户向外看
-// 妈妈能开到球从窗口经过几次（包括球落下和弹起的情况）
+// 妈妈能看到球从窗口经过几次（包括球落下和弹起的情况）
 
+// 内部函数递归
 const bouncingBall = (h, bounce, window) => {
-  let count = 0
-  if (h <= window) return -1
-  if (h <= 0) return -1
-  if (bounce >= 1 || bounce <= 0) return -1
+  let count = 1
+  if (h <= window || h <= 0 || bounce >= 1 || bounce <= 0) return -1
   // 第一次落下
-  count++
   const iterate = (h) => {
     h = h * bounce
     if (h > window) {
@@ -28,3 +26,29 @@ const bouncingBall = (h, bounce, window) => {
 }
 
 console.log(bouncingBall(3, 0.66, 1.5))
+
+// 通过while循环来实现
+const bouncingBall2 = (h, bounce, window) => {
+  let count = 1 // 由于要求h > window,所以第一次下落的时候必定会被看到
+  if (h <= window || h <= 0 || bounce >= 1 || bounce <= 0) return -1
+  // 第一次落下
+  h = h * bounce
+  while (h > window) {
+    // 如果h > window，那么说明前一次下落和这一次弹起时必定会被看到
+    count = count + 2
+    h = h * bounce
+  }
+  return count
+}
+console.log(bouncingBall2(3, 0.66, 1.5))
+
+// 通过递归调用定义的函数: 类似于斐波那契数列
+const bouncingBall3 = (h, bounce, window) => {
+  // 最终会由于 h < window 而return -1
+  // 之后会
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+    return -1;
+  }
+  var newHeight = h * bounce;
+  return bouncingBall(newHeight, bounce, window) + 2;
+}
